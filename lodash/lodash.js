123b2589f2b2c2275f14006cebefa89e2338b100
edxan77 <edgar.khanamiryan@gmail.com>
2022-01-27 14:49:22 +0400

    homework

---
+++ lodash/lodash.js
@@ -0,0 +1,16 @@
+const arrayA = [1, 2, 3, 4, 5]
+const arrayB = [4,8,5]
+console.log(_.intersection(arrayA,arrayB))
+console.log(arrayB)
+
+const arr = ['a', 'b', 'c', 'a', 'b', 'c']
+_.pull(arr, 'a', 'b',)
+
+
+
+const mas = [1,2,6,4,6]
+_.take(mas,3) // returned Array without 4,6
+
+const zangvac = [1,3,5,4]
+_.tale(zangvac)   //returned Array without 1
+
